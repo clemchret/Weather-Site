acabe96791dd97b1a3eb5e9862188feb035bf27a
clemchret <clement.chret@gmail.com>
2023-03-09 02:14:03 +0100

    Weather site pre v2

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const hourlyWeather = document.querySelector(".hourly-weather");
 const map = document.querySelector(".rain-map");
 const emptyCity = document.querySelector(".no-city-error");
 
+
+//Chargement des villes dans le menu latéral
+window.onload = function(){
+    menuFavouritesDisplay();
+}
+
+
+//Interaction avec le formulaire
 form.addEventListener("submit", e => {
     e.preventDefault();
 
@@ -17,6 +25,8 @@ form.addEventListener("submit", e => {
     }
 });
 
+
+//Fonction récupérant et affichant la météo en fonction de la ville rentrée
 const apiKey = "246a4b38d08f488b11e50c20008365cb";
 
 const getCurrentWeather = async() => {
@@ -51,6 +61,15 @@ const getCurrentWeather = async() => {
 
     const icon = `https://openweathermap.org/img/wn/${cityData.weather[0]["icon"]}@2x.png`;
 
+
+    //Vérifier si la ville actuelle est dans la liste des villes favorites stockées dans le localStorage
+    const favorites = JSON.parse(localStorage.getItem('Ville')) || [];
+    const isFavorite = favorites.includes(cityData.name);
+
+    //Modifier la classe de l'icône en conséquence
+    const starIconClass = isFavorite ? 'fa-solid favourited' : 'fa-regular';
+    const starIcon = `<i class="${starIconClass} fa-star" title="Favoris"></i>`;
+
     main.innerHTML = 
     `
     <div class="row">
@@ -60,16 +79,17 @@ const getCurrentWeather = async() => {
         </div>
         <div class="col-6 text-right">
             <div class="city-description-container">
-                <div class="city">${cityData.name}, ${cityData.sys.country}</div>
+                <div class="city">${cityData.name}, ${cityData.sys.country} ${starIcon}</div>
                 <div class="description">${cityData.weather[0].description}</div>
             </div>
         </div>
         <div class="col-12 text-center">
-            <button class="refresh" type="button" onclick="getImages()"><i class="fa-solid fa-rotate"></i> ${hours}</button>
+            <button class="refresh" type="button"><i class="fa-solid fa-rotate"></i> ${hours}</button>
         </div>
     </div>
     `;
 
+    /*//Bouton refresh
     const refreshBtn = document.querySelector(".refresh");
     refreshBtn.addEventListener('mouseenter', function add() {
         this.querySelector('.fa-rotate').classList.add('fa-spin');
@@ -78,6 +98,10 @@ const getCurrentWeather = async() => {
     refreshBtn.addEventListener('mouseleave', function remove() {
         this.querySelector('.fa-rotate').classList.remove('fa-spin');
     });
+
+    refreshBtn.addEventListener("click", function() {
+        getCurrentWeather();
+    });*/
 
 
     details.innerHTML = 
@@ -129,4 +153,59 @@ const getCurrentWeather = async() => {
 
     form.reset();
     input.focus();
+
+
+    //Fonction d'ajout et de suppressiond'une ville dans le localstorage
+    //Je me base sur la classe de l'icone pour décider de quelle fonction call : fa-regular = ajout de ville, fa-solid = suppression
+    const favouriteButton = document.querySelector(".city .fa-star");
+    favouriteButton.addEventListener('click', e => {
+        e.preventDefault();
+
+        //Si la classe fa-regular est présente, cela veut dire que la ville n'est pas enregistrée et qu'il faut donc l'ajouter
+        if (favouriteButton.classList.contains('fa-regular')) {
+
+            localStorage.setItem("Ville", JSON.stringify([...new Set([...JSON.parse(localStorage.getItem("Ville") || "[]"), cityData.name])]));
+            favouriteButton.classList.toggle('fa-regular');
+            favouriteButton.classList.toggle('fa-solid');
+            menuFavouritesDisplay();
+
+        //L'inverse ici
+        } else if (favouriteButton.classList.contains('fa-solid')) {
+
+            let villes = JSON.parse(localStorage.getItem("Ville")) || [];
+            const index = villes.indexOf(cityData.name);
+            if (index !== -1) {
+              villes.splice(index, 1);
+              localStorage.setItem("Ville", JSON.stringify(villes));
+            }
+            favouriteButton.classList.toggle('fa-regular');
+            favouriteButton.classList.toggle('fa-solid');
+            menuFavouritesDisplay();
+        };
+    });
 };
+
+
+//Fonction qui charge les villes stockées dans le locelstorage au chargement de la page mais aussi lors de l'ajout ou suppression de la ville
+function menuFavouritesDisplay (){
+    const villes = JSON.parse(localStorage.getItem("Ville")) || [];
+    const navbar = document.querySelector(".navbar-nav");
+    //Ligne suivante pas nécessaire lors du chargement au début mais si la fonction ajout/suppression est utilisée, cela permet de clear les villes chargées au début et d'afficher la nouvelle liste
+    navbar.innerHTML="";
+
+    if (villes.length === 0) {
+        navbar.innerHTML = 
+        `
+          <p>Aucune ville favorite pour le moment !</p>
+        `;
+    }else {
+        villes.forEach(ville => {
+            navbar.innerHTML +=
+            `
+            <li class="nav-item">
+                <a class="nav-link href="#">${ville}</a>
+            </li>
+            `;
+        });
+    }
+}
